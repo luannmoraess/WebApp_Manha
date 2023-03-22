81b00397dc7d069c36aed70c8ef3c91c5be4cb5b
Ferneko <fcgraciano@gmail.com>
2023-03-22 08:53:32 -0300

    Add project files.

---
+++ WebApp_Manha/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp_Manha.Models;
+
+namespace WebApp_Manha.Controllers
+{
+    public class HomeController : Controller
+    {
+        //atributo da classe HomeController
+        private readonly ILogger<HomeController> _logger;
+
+        //Construtor da classe
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Action Result devolve uma tela (Html)
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Mais uma tela no sistema
+        public IActionResult BemVindo()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp_Manha/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_Manha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
