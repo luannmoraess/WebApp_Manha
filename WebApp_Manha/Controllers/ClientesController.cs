c44a566df3952b4a3e88e767650f036097ed7094
Ferneko <fcgraciano@gmail.com>
2023-03-29 11:03:43 -0300

    Cadastro de erro e cliente

--- WebApp_Manha/Controllers/ClientesController.cs
+++ WebApp_Manha/Controllers/ClientesController.cs
@@ -8,5 +8,39 @@ namespace WebApp_Manha.Controllers
         {
             return View();
         }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone)
+        {
+            if (string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "O campo nome não pode estar em branco";
+            }
+
+            if (string.IsNullOrEmpty(Telefone))
+            {
+                TempData["Erro"] = "O campo Telefone não pode estar em branco";
+            }
+            return View();
+        }
+
+
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
+        public IActionResult Compras(int id)
+        {
+            return View();
+        }
     }
 }
