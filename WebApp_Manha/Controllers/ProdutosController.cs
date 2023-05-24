7be08e02b9cef15076818d58590f55664676e454
luannmoraess <luanmoraes10_@hotmail.com>
2023-05-24 10:35:14 -0300

    .

--- WebApp_Manha/Controllers/ProdutosController.cs
+++ WebApp_Manha/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp_Manha.Entidades;
 using WebApp_Manha.Models;
 
@@ -16,14 +17,15 @@ namespace WebApp_Manha.Controllers
 
         public IActionResult Lista()
         {
-            return View();
+            List<Produtos> model = new List<Produtos>(); 
+            model = db.Produtos.Include(a => a.Categoria).ToList();
+            return View( model );
         }
 
         public IActionResult Cadastro()
         {
             NovoProdutoModelView model = new NovoProdutoModelView();
             model.ListaCategorias = db.Categorias.ToList();
-            int i = 0;
             return View(model);
         }
         [HttpPost]
